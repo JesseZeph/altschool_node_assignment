595213864a494968164ba4c65575e7e90cd103f5
Jesse Josiah <jessezephyr@gmail.com>
2023-07-31 23:21:19 +0100

    done

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World');
+})
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
+    
